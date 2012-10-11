dac7ad36d80c1c4488a6da2511576e94bbe574c9
Terry Brown <terry@hairylemon.net>
2012-10-11 22:40:14 +0100

    Setting up question, speaker and presenter templates - no significant work, just basic layout

---
+++ src/InterASK/Controllers/PresenterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterASK.Controllers
+{
+    public class PresenterController : Controller
+    {
+        //
+        // GET: /Presenter/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/InterASK/Scripts/site.js
@@ -0,0 +1,3 @@
+﻿$(function () {
+	$('[rel="tooltip"]').tooltip();
+});
